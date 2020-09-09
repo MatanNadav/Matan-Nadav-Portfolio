31e1c6a98c4970040ead2e12b06757937f396c7b
Matan Nadav <Smatannadav@gmail.com>
2020-09-09 16:43:34 +0300

    new images

--- js/proj-service.js
+++ js/proj-service.js
@@ -2,33 +2,34 @@
 
 var gProjs;
 
-console.log(gProjs);
+// console.log(gProjs);
 
 function createProjs() {
     var projs = [
 
         createProj('Connect 4','Game',
-            'The familiar game with a different flair.',
+            'The familiar game using HTML5 canvas with animations and dynamically generated game. Game logic & design',
              'img/myPic/Connect 4 - thumb.png','img/myPic/Connect 4.png', 'https://matannadav.github.io/four-in-a-row/',['Recursion']),
 
         createProj('Eventera','Events sharing platform',
             'An event management & sharing platform, complete with a powerful server',
             'img/myPic/Eventera - thumb.png','img/myPic/Eventera.png','http://eventera.herokuapp.com/' ,['Inputs', 'Selectors']),
 
-        createProj('MemeGen', 'Meme Generator',
-            'A Meme Generator using HTML5 Canvas & Vanilla JS',
-            'img/myPic/MemeGen - thumb.png','img/myPic/MemeGen.png', 'https://matannadav.github.io/meme-gen/',['Loops', 'Globals']),
         createProj('HeroWeather', 'Weather and Photos of locations around the world',
-            'Something i wanted to develop for some time. Weather and Photos of places around the world with a flair',
+            'Weather and Photos of places around the world complete with client, server and DB',
             'img/myPic/HeroWeather - thumb.jpg','img/myPic/HeroWeather.jpg', 'https://github.com/MatanNadav/HeroWeather',['Vue', 'Node.js']),
 
         createProj('Appsus', 'Email & Notes',
-            'Leaning on the familiar gmail, i developed a quirky email & notes app with a lot of features',
+            'One of my earliest projects, mainly focusing on design & css',
             'img/myPic/Appsus - thumb.png','img/myPic/Appsus.png','https://matannadav.github.io/AppSus/#/',['Arrays', 'Loops']),
 
         createProj('E-store', 'Clothing and apparel store',
-            'Dynamically generated HTML components and responsivity',
+            'Dynamically generated HTML components. Cart & Checkout features',
             'img/myPic/e-store - thumb.png','img/myPic/e-store.png', 'https://github.com/MatanNadav/E-Commerce-FED',['Jquery', 'bootstrap']),
+
+        createProj('My Repo', 'GitHub repo',
+            'GitHub repo with many different things i developed',
+            'img/myPic/github-image - thumb.png','img/myPic/github-image.png', 'https://github.com/MatanNadav',['Loops', 'Globals']),
 
     ]
     gProjs = projs;
